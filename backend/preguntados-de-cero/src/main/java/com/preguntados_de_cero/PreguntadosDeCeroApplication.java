6d458a8b556e7391b90a62d170297eb5a86229e2
ramirotolosa <ramirotolosa99@gmail.com>
2026-01-08 20:54:27 -0300

    Estructura basica repositorio

---
+++ backend/preguntados-de-cero/src/main/java/com/preguntados_de_cero/PreguntadosDeCeroApplication.java
@@ -0,0 +1,13 @@
+package com.preguntados_de_cero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntadosDeCeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntadosDeCeroApplication.class, args);
+	}
+
+}
---
+++ backend/preguntados-de-cero/src/test/java/com/preguntados_de_cero/PreguntadosDeCeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preguntados_de_cero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntadosDeCeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
