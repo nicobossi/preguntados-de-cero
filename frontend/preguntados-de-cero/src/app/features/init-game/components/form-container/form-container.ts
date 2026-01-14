b6ce2cf85e92ace8fa93910d099355db726ce70b
ramirotolosa <ramirotolosa99@gmail.com>
2026-01-14 05:07:18 -0300

    Continuacion refactor init-page. Falta corregir responsive y agregar el service que guarde el nombre en una variable

--- frontend/preguntados-de-cero/src/app/features/init-game/components/form-container/form-container.ts
+++ frontend/preguntados-de-cero/src/app/features/init-game/components/form-container/form-container.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {InitLogo} from '@/app/features/init-game/components/init-logo/init-logo';
 import {NameForm} from '@/app/features/init-game/components/name-form/name-form';
+import {InitDesktopImage} from '@/app/features/init-game/components/init-desktop-image/init-desktop-image';
 
 @Component({
   selector: 'app-form-container',
   imports: [
     InitLogo,
     NameForm,
+    InitDesktopImage,
   ],
   templateUrl: './form-container.html',
   styleUrl: './form-container.css',
