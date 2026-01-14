bc78be156b3ae28f0e1fd8db6703d5b33d9cd005
ramirotolosa <ramirotolosa99@gmail.com>
2026-01-14 18:12:22 -0300

    Fin refactor estilos en init-game. Falta hacer service de contexto para el nombre de jugar

--- frontend/preguntados-de-cero/src/app/features/init-game/init-game.ts
+++ frontend/preguntados-de-cero/src/app/features/init-game/init-game.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {AppHeader} from '@/app/shared/components/app-header/header';
 import {FormContainer} from '@/app/features/init-game/components/form-container/form-container';
+import {InitLogo} from '@/app/features/init-game/components/init-logo/init-logo';
 
 @Component({
   selector: 'app-init-game',
   imports: [
     AppHeader,
-    FormContainer
+    FormContainer,
+    InitLogo
   ],
   templateUrl: './init-game.html',
   styleUrl: './init-game.css',
