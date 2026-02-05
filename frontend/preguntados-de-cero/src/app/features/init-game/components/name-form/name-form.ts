4f249e4966eae03892c5e8c8ea1f12954313ac66
Nicolás Bossi <nbossi@hotmail.com>
2026-02-05 17:42:40 -0300

    Se refactorizó el formulario de la página inicial con dos servicios

--- frontend/preguntados-de-cero/src/app/features/init-game/components/name-form/name-form.ts
+++ frontend/preguntados-de-cero/src/app/features/init-game/components/name-form/name-form.ts
@@ -1,39 +1,28 @@
 import {Component, inject} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import {PlayButton} from '@/app/features/init-game/components/play-button/play-button';
 import {PlayerName} from '@/app/core/services/player-name/player-name';
-import {Router} from '@angular/router';
+import { FormErrorsContainer } from "../form-errors-container/form-errors-container";
+import { InitForm } from '../../services/init-form.service';
+import { InputName } from "../input-name/input-name";
 
 @Component({
   selector: 'app-name-form',
   imports: [
     PlayButton,
     ReactiveFormsModule,
-  ],
+    FormErrorsContainer,
+    InputName
+],
   templateUrl: './name-form.html',
   styleUrl: './name-form.css',
 })
 export class NameForm {
-  private router = inject(Router);
+
   private nameService = inject(PlayerName);
-
-  nameform = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-    ]),
-  })
-
-  get name() {
-    return this.nameform.controls.name;
-  }
+  protected initFormService = inject(InitForm);
 
   protected onSubmit() {
-    if (this.nameform.valid) {
-      this.nameService.setName(this.name.value!);
-      console.log('Nombre guardado: ', this.nameService.getName()); /*debug hasta que se implemente la pagina de review fin de juego*/
-      this.router.navigate(['/level']);
-    }
+    if (this.initFormService.isValidForm()) this.nameService.saveName(this.initFormService.getValue);
   }
 }
