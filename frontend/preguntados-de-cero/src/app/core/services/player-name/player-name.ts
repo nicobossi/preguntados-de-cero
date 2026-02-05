d384a8764879139c48c44061c1b527aa6cbcca01
Nicolás Bossi <nbossi@hotmail.com>
2026-02-05 18:11:11 -0300

    Se agregó un servicio para el formulario

--- frontend/preguntados-de-cero/src/app/core/services/player-name/player-name.ts
+++ frontend/preguntados-de-cero/src/app/core/services/player-name/player-name.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerName {
   private name: string = "";
+  private router : Router = inject(Router);
 
-  setName(name: string): void{
+  setName(name: string): void {
     this.name = name;
   }
 
-  getName(): string{
+  get getName(): string{
     return this.name;
+  }
+
+  saveName(name: string): void {
+    this.setName(name);
+    console.log("Name " + this.getName)
+    this.router.navigate(["/level"]);
   }
 }
