f874ccc4fb07eef555283b07cbb0d049e4927b4a
Nicolás Bossi <nbossi@hotmail.com>
2026-02-19 16:22:19 -0300

    Se quitó el header de la página del nivel

--- frontend/preguntados-de-cero/src/app/features/level/level.ts
+++ frontend/preguntados-de-cero/src/app/features/level/level.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { AppHeader } from '@/app/shared/components/app-header/header';
 import { LevelCardContainer } from './components/level-card-container/level-card-container';
 import { LevelService } from './services/level/level.service';
 import { LevelSelectorService } from './services/level-selector/level-selector.service';
 
 @Component({
   selector: 'app-level',
-  imports: [AppHeader, LevelCardContainer],
+  imports: [LevelCardContainer],
   providers: [LevelService, LevelSelectorService],
   templateUrl: './level.html',
   styleUrl: './level.css'
