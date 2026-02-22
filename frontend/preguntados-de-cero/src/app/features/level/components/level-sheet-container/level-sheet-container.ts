d756b74ce430f9521dc93aaeaa4d7c44f5e888ea
Nicolás Bossi <nbossi@hotmail.com>
2026-02-22 18:09:47 -0300

    Se colocó los botones para pasar del nivel sobre el contenedor del botón para seleccionar el nivel

--- frontend/preguntados-de-cero/src/app/features/level/components/level-sheet-container/level-sheet-container.ts
+++ frontend/preguntados-de-cero/src/app/features/level/components/level-sheet-container/level-sheet-container.ts
@@ -3,10 +3,11 @@ import { LevelSheet  } from '../level-sheet/level-sheet';
 import Difficulty from '@/app/shared/types/difficulty';
 import { LevelSelectorService } from '../../services/level-selector/level-selector.service';
 import { ThemesNameCard } from '../themes-name-card/themes-name-card';
+import { MovementButtons } from "../movement-buttons/movement-buttons";
 
 @Component({
   selector: 'app-sheet-container',
-  imports: [LevelSheet, ThemesNameCard],
+  imports: [LevelSheet, ThemesNameCard, MovementButtons],
   templateUrl: './level-sheet-container.html',
   styleUrl: './level-sheet-container.css',
 })
--- frontend/preguntados-de-cero/src/app/features/level/level.ts
+++ frontend/preguntados-de-cero/src/app/features/level/level.ts
@@ -2,11 +2,10 @@ import { Component, inject } from '@angular/core';
 import { LevelSheetContainer } from './components/level-sheet-container/level-sheet-container';
 import { LevelService } from './services/level/level.service';
 import { LevelSelectorService } from './services/level-selector/level-selector.service';
-import { MovementButtons } from "./components/movement-buttons/movement-buttons";
 
 @Component({
   selector: 'app-level',
-  imports: [LevelSheetContainer, MovementButtons],
+  imports: [LevelSheetContainer],
   providers: [LevelService, LevelSelectorService],
   templateUrl: './level.html',
   styleUrl: './level.css'
