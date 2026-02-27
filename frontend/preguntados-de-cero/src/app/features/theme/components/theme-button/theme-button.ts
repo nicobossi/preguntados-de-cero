519093878f72ff06d36b7f1b579bb3bab15c5207
Nicolás Bossi <nbossi@hotmail.com>
2026-02-27 15:32:49 -0300

    Se creó un hijo directo con css para agregar la animación en el botón de la tematica, quitando los bordes de todos los lados

--- frontend/preguntados-de-cero/src/app/features/theme/components/theme-button/theme-button.ts
+++ frontend/preguntados-de-cero/src/app/features/theme/components/theme-button/theme-button.ts
@@ -1,17 +1,14 @@
-import {Component, inject, Input, WritableSignal} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import GameTheme from '@/app/shared/types/gameTheme';
-import {NgClass} from '@angular/common';
 import { NavegateService } from '@/app/core/services/navegate/navegate.services';
 
 @Component({
   selector: 'app-theme-button',
-  imports: [NgClass],
   templateUrl: './theme-button.html',
   styleUrl: './theme-button.css',
 })
 export class ThemeButton {
   @Input() theme! : GameTheme;
-  @Input() isDescriptionVisible! : WritableSignal<boolean>;
   private navegateService = inject(NavegateService);
 
   protected navegate() {
