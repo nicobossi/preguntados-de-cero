506a4f2d827c9f043621700d6a6651e8c63902e1
Nicolás Bossi <nbossi@hotmail.com>
2026-03-04 17:26:31 -0300

    Se creó el componente de carga y se implementó en la página de niveles, tematicas y para el comienzo del juego

--- frontend/preguntados-de-cero/src/app/features/level/level.ts
+++ frontend/preguntados-de-cero/src/app/features/level/level.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { LevelSheetContainer } from './components/level-sheet-container/level-sheet-container';
 import { LevelService } from './services/level/level.service';
 import { LevelSelectorService } from './services/level-selector/level-selector.service';
+import { StateLoader } from '@/app/shared/components/state-loader/state-loader';
 
 @Component({
   selector: 'app-level',
-  imports: [LevelSheetContainer],
+  imports: [LevelSheetContainer, StateLoader],
   providers: [LevelService, LevelSelectorService],
   templateUrl: './level.html',
   styleUrl: './level.css'
