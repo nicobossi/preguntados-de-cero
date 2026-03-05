317fe15ae2a55468a51a93a02fa4939b4825cac8
Nicolás Bossi <nbossi@hotmail.com>
2026-03-04 22:04:33 -0300

    Se achicó el ancho del contenedor general en resolución mobil

--- frontend/preguntados-de-cero/src/app/core/pages/error-page/components/card/card.ts
+++ frontend/preguntados-de-cero/src/app/core/pages/error-page/components/card/card.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import {ErrorCode} from '@/app/core/pages/error-page/components/error-code/error-code';
 import {ErrorDescription} from '@/app/core/pages/error-page/components/error-description/error-description';
 import { ButtonNavegate } from "@/app/features/end-game/components/button-navegate/button-navegate";
-import { Router } from '@angular/router';
 import { NavegateService } from '@/app/core/services/navegate/navegate.services';
 
 @Component({
@@ -16,7 +15,6 @@ import { NavegateService } from '@/app/core/services/navegate/navegate.services'
   styleUrl: './card.css',
 })
 export class Card {
-  private router = inject(Router);
   private navegate = inject(NavegateService);
 
   onClick() {
