216ade4bcf96b5b13b425e71f254e77c72e13bc0
Nicolás Bossi <nbossi@hotmail.com>
2026-03-04 21:57:14 -0300

    Se utilizó el botón de navegación compartido

--- frontend/preguntados-de-cero/src/app/core/pages/error-page/components/card/card.ts
+++ frontend/preguntados-de-cero/src/app/core/pages/error-page/components/card/card.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {ErrorCode} from '@/app/core/pages/error-page/components/error-code/error-code';
 import {ErrorDescription} from '@/app/core/pages/error-page/components/error-description/error-description';
-import {HomeButton} from '@/app/core/pages/error-page/components/home-button/home-button';
+import { ButtonNavegate } from "@/app/features/end-game/components/button-navegate/button-navegate";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   imports: [
     ErrorCode,
     ErrorDescription,
-    HomeButton
-  ],
+    ButtonNavegate
+],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
 export class Card {
+  private router = inject(Router);
 
+  onClick() {
+    this.router.navigate(['']);
+  }
 }
