c4b59ecaa0c0eaa922ab950a53b80c98a6684dc0
ramirotolosa <ramirotolosa99@gmail.com>
2026-03-06 00:23:10 -0300

    Fix configuracion CORS del back

--- backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/PreguntadosDeCeroApplication.java
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/PreguntadosDeCeroApplication.java
@@ -18,7 +18,13 @@ public class PreguntadosDeCeroApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://earnest-quietude-production-d60d.up.railway.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
