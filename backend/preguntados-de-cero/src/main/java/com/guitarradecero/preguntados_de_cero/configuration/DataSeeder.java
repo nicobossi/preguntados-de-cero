16f7e3b8ce68b2c73e496b431312459045847554
ramirotolosa <ramirotolosa99@gmail.com>
2026-03-06 00:16:25 -0300

    deshabilitado el data seeder para evitar error en el servidor

--- backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/configuration/DataSeeder.java
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/configuration/DataSeeder.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class DataSeeder implements CommandLineRunner {
 
