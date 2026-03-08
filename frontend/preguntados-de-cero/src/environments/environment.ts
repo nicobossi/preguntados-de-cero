54d95a25316ca011fd094bc448926ba1ea992231
Nicolás Bossi <nbossi@hotmail.com>
2026-03-08 16:17:14 -0300

    Se agrandaron los contenedores de los contadores

--- frontend/preguntados-de-cero/src/environments/environment.ts
+++ frontend/preguntados-de-cero/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
   production: true,
-  apiBackendUrl: 'https://preguntados-de-cero-deploy-production.up.railway.app' // para docker-compose cambiar por localhost:8080
+  apiBackendUrl: 'http://localhost:8080' // para docker-compose cambiar por localhost:8080
 
 };
