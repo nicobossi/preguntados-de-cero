cccf204b69856cfcf7cc396e0e1855bc14235550
ramirotolosa <ramirotolosa99@gmail.com>
2026-03-08 16:20:56 -0300

    Saco negritas de los textos

--- frontend/preguntados-de-cero/src/environments/environment.ts
+++ frontend/preguntados-de-cero/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
 
   production: true,
-  apiBackendUrl: 'https://preguntados-de-cero-deploy-production.up.railway.app' // para docker-compose cambiar por localhost:8080
+  // apiBackendUrl: 'https://preguntados-de-cero-deploy-production.up.railway.app' // para docker-compose cambiar por localhost:8080
+  apiBackendUrl: 'http://localhost:8080'
 
 };
