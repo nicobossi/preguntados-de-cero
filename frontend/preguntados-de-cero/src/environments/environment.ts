70a9645d401978fe0b8e82ccaa38c3669e1435a4
ramirotolosa <ramirotolosa99@gmail.com>
2026-03-14 21:10:18 -0300

    fix url backend

--- frontend/preguntados-de-cero/src/environments/environment.ts
+++ frontend/preguntados-de-cero/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
   production: true,
-  // apiBackendUrl: 'https://preguntados-de-cero-deploy-production.up.railway.app'
-  apiBackendUrl: 'http://localhost:8080' // para docker-compose cambiar por localhost:8080
+  // apiBackendUrl: 'https://preguntados-de-cero-deploy-production.up.railway.app' // docker-compose railway deploy
+  apiBackendUrl: 'http://localhost:8080' // docker-compose local
 
 };
