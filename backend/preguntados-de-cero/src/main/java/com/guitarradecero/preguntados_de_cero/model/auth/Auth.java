700a10be66a818101be742a36228309955e4b24e
Nicolás Bossi <nbossi@hotmail.com>
2026-07-26 14:43:56 -0300

    Se quitaron los filtrados mal configurados del mapper para la autenticación

--- backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/model/auth/Auth.java
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/model/auth/Auth.java
@@ -2,8 +2,8 @@ package com.guitarradecero.preguntados_de_cero.model.auth;
 
 public record Auth(
         String email,
+        Long id,
         String password,
-        String token,
-        Long id
+        String token
 ) {
 }
