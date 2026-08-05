97fbfe44acee5af6920de6ac61315f491d7984b2
Nicolás Bossi <nbossi@hotmail.com>
2026-08-05 17:11:10 -0300

    Se creó la página del login para reutilizar el formulario y se creó la ruta para dicha página

---
+++ frontend/preguntados-de-cero/src/app/features/login/login.ts
@@ -0,0 +1,12 @@
+import { FormContainer } from '@/app/shared/components/form-container/form-container';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormContainer],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
