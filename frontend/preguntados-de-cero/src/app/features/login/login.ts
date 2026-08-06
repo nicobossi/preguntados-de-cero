3270393b329eb898de49e9d59accc0eb3ef3a9b7
Nicolás Bossi <nbossi@hotmail.com>
2026-08-05 22:09:40 -0300

    Se agregó el formulario al contenedor prinicpal del login

--- frontend/preguntados-de-cero/src/app/features/login/login.ts
+++ frontend/preguntados-de-cero/src/app/features/login/login.ts
@@ -1,10 +1,11 @@
 import { FormContainer } from '@/app/shared/components/form-container/form-container';
 import { Component } from '@angular/core';
 import { ButtonContainer } from './components/button-container/button-container';
+import { Form } from "./components/form/form";
 
 @Component({
   selector: 'app-login',
-  imports: [FormContainer, ButtonContainer],
+  imports: [FormContainer, ButtonContainer, Form],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
