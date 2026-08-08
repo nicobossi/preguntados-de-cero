cbcecdb12461e0cfae41d2c48503f0570acea4ce
Nicolás Bossi <nbossi@hotmail.com>
2026-08-08 15:39:59 -0300

    Se creó un controller, un mapper y un dto para obtener los datos del usuario autenticado

---
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/controller/user/UserController.java
@@ -0,0 +1,27 @@
+package com.guitarradecero.preguntados_de_cero.controller.user;
+
+import com.guitarradecero.preguntados_de_cero.adapter.user.UserMapper;
+import com.guitarradecero.preguntados_de_cero.dto.user.UserResponseDto;
+import com.guitarradecero.preguntados_de_cero.model.user.User;
+import com.guitarradecero.preguntados_de_cero.security.user.UserDetailsImpl;
+import com.guitarradecero.preguntados_de_cero.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/user")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> me(Authentication param) {
+        UserDetailsImpl auth = (UserDetailsImpl) param.getPrincipal();
+        User user = userService.getById(auth.getId());
+        return ResponseEntity.ok(UserMapper.INSTANCE.modelToDto(user));
+    }
+}
---
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/dto/user/UserResponseDto.java
@@ -0,0 +1,7 @@
+package com.guitarradecero.preguntados_de_cero.dto.user;
+
+public record UserResponseDto(
+        Long id,
+        String email
+) {
+}
