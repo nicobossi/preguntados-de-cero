43ed2f3ab82dcd9af7d9a864224e2d01360e4e1a
Nicolás Bossi <nbossi@hotmail.com>
2026-08-09 13:30:47 -0300

    Se atrapó la excepción del servicio jwt desde el filtro para poder ejecutar el entrypoint, además se habilitaron las rutas del usuario para refrescar el frontend al recargar la página

--- backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/configuration/JwtFilterConfig.java
+++ backend/preguntados-de-cero/src/main/java/com/guitarradecero/preguntados_de_cero/configuration/JwtFilterConfig.java
@@ -5,10 +5,14 @@ import com.guitarradecero.preguntados_de_cero.security.filters.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class JwtFilterConfig {
@@ -26,15 +30,18 @@ public class JwtFilterConfig {
     public SecurityFilterChain chainJwtFilter(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/api/**")
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 .requestMatchers("/api/difficulty/add").authenticated()
                                 .requestMatchers("/api/theme/add/*").authenticated()
                                 .requestMatchers("/api/question/add/*").authenticated()
+                                .requestMatchers("/api/user/me").authenticated()
                                 .anyRequest().permitAll())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(tokenException))
-                .addFilterAfter(jwtFilter, ExceptionTranslationFilter.class);
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
